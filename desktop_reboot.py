c23002700b0a30e8b002d1169965e8fcf0d991fd
vhirtzel <vincent@browserstack.com>
2021-07-21 08:06:56 -0700

    Clean up code to comply with PEP8 and adding a simple Rich based TUI

--- desktop_reboot.py
+++ desktop_reboot.py
@@ -1,89 +1,140 @@
 import re
-import sys
-import os
 import configparser
-'''
-In order to use the latest Raritan SDK API bindings I've included it in this repo and the following code will add it to the path so it can be imported.
-'''
-api_path = os.path.abspath(os.path.join('.','pdu-python-api'))
-sys.path.append(api_path)
+
+from rich.console import Console
+from rich.live import Live
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.table import Table
+from rich.prompt import Prompt
+
+
 import raritan.rpc
 from raritan.rpc import pdumodel
-'''
-This is a basic regex pattern that will match any of the Desktop Machines in our DC because they are the only machines on these IP ranges.
-This doesn't address invalid final octets but that is handled in the while loop of the main function.
-'''
-valid_ip = re.compile(r'65.74.(185|147).(\d*)')
 
-'''
-The main function uses a while loop to ask the user to enter the IP addresses of machines that need to be rebooted.
-'''
+console = Console()
+layout = Layout()
+
+# Divide the "screen" in to three parts
+layout.split(
+    Layout(name="header", size=3),
+    Layout(ratio=1, name="main"),
+    Layout(size=10, name="user_prompts"),
+)
+# Divide the "main" layout in to "side" and "body"
+layout["main"].split_row(
+    Layout(name="ip_list"),
+    Layout(name="status", ratio=2),
+)
+layout["header"].update(
+    Layout(
+        Panel(
+            "Desktop Machine Reboot",
+        )
+    )
+)
+
+ip_table = Table(expand=True)
+ip_table.add_column("IPs")
+layout["ip_list"].update(Layout(ip_table))
+
+status_table = Table(expand=True)
+status_table.add_column("Status")
+layout["status"].update(Layout(status_table))
+
+# Regex to capture valid machine IPs
+valid_ip = re.compile(r"65.74.(185|147).(\d*)")
+
+
 def main():
-    print("Enter IP addresses of machines needing to be rebooted.") # Print out instructions for the user
-    print("Type DONE when finished.") 
+    console.print(layout)
     ip_lst = []
     config = parseConfig()
     user = getUser(config)
-    passwd = getPass(config)                                        
+    passwd = getPass(config)
     hosts = getHosts(config)
-    while True:                                                     
-        entry = input("")                                           # Collect a line of input from the user (this can be a copy pasted group of IPs if each IP is on a new line)
-        mo = re.search(valid_ip, entry)                             # Using the regex defined above, search the user input for an IP pattern
-        if entry == "DONE":                                         # This ends the loop when the user types DONE
+    layout["user_prompts"].update(
+        Layout(
+            Panel(
+                "Enter IP addresses of machines needing to be rebooted"
+            )
+        )
+    )
+    console.print(layout)
+    while True:
+        entry = Prompt.ask("Type DONE when finished")
+        mo = re.search(valid_ip, entry)
+        if entry == "DONE":
             break
-        elif (mo != None):                                          # This checks if the regex search was successful
-            last_octete = mo.groups()[1]                            # Pulls out the last octet of the IP address
-            last_octet_int = int(last_octete)                       # Casts the last octet into an integer so that we can compare sizes
-            if (mo.groups()[0] == "185") and last_octet_int < 255:  # Checks first if the subnet is 185 then checks that the last octet is less than 255
-                ip_lst.append(entry)                                # Adds the IP address string to the ip_lst list variable if it passes the check
-            elif (mo.groups()[0] == "147") and last_octet_int < 30: # This checks if the subnet is 147 then checks if the last octet is less than 30 since 29 is the last IP we've added.
-                ip_lst.append(entry)                                # Adds the IP address string to the ip_lst list variable if it passes the check
-            else:                                                   
+        elif mo is not None:
+            last_octete = mo.groups()[1]
+            last_octet_int = int(last_octete)
+            if (mo.groups()[0] == "185") and last_octet_int < 255:
+                ip_lst.append(entry)
+                ip_table.add_row(entry)
+            elif (mo.groups()[0] == "147") and last_octet_int < 40:
+                ip_lst.append(entry)
+                ip_table.add_row(entry)
+            else:
                 print("Invalid entry, please enter an IP or type DONE")
         else:
-            print("Invalid entry, please enter an IP or type DONE") 
+            print("Invalid entry, please enter an IP or type DONE")
+    with Live(layout, refresh_per_second=4):
+        powercycle(user, passwd, hosts, ip_lst)
 
-    powercycle(user, passwd, hosts, ip_lst)
 
 def parseConfig():
     config = configparser.ConfigParser()
-    config.read('configfile.ini')
+    config.read("configfile.ini")
+    status_table.add_row("Reading Config... ✅")
     return config
 
+
 def getUser(config):
-    user = config['Credentials']['user']
+    user = config["Credentials"]["user"]
+    status_table.add_row("Getting Raritan PDU Username... ✅")
     return user
 
+
 def getPass(config):
-    passwd = config['Credentials']['passwd']
+    passwd = config["Credentials"]["passwd"]
+    status_table.add_row("Getting Raritan PDU Password... ✅")
     return passwd
+
 
 def getHosts(config):
     hosts = []
-    for key in config['Hosts']:
-        hosts.append(config['Hosts'][key])
+    for key in config["Hosts"]:
+        hosts.append(config["Hosts"][key])
+    status_table.add_row("Getting Raritan PDU IPs... ✅")
     return hosts
+
 
 def powercycle(user, passwd, hosts, ips):
     for host in hosts:
         if len(ips) == 0:
-            print('All machines rebooted successfully')
+            layout["user_prompts"].update(
+                Layout(Panel("All machines rebooted successfully!"))
+            )
             exit()
-        print(f'Connecting to {host}...')
-        agent = raritan.rpc.Agent("https", host, user, passwd, disable_certificate_verification = True)
+        status_table.add_row(f"Connecting to {host}...")
+        agent = raritan.rpc.Agent(
+            "https", host, user, passwd, disable_certificate_verification=True
+        )
         pdu = pdumodel.Pdu("model/pdu/0", agent)
-        print(f'Collecting outlet data...')
+        status_table.add_row("Collecting outlet data...")
         outlets = pdu.getOutlets()
-        print('Getting outlet names and checking against list of IPs')
+        status_table.add_row("Checking outlet data against list of IPs")
         for outlet in outlets:
             name = outlet.getSettings().name
             if name in ips:
-                print(f'Rebooting {name}')
+                status_table.add_row(f"Rebooting {name}")
                 outlet.cyclePowerState()
                 ips.remove(name)
                 if len(ips) == 0:
-                    print('All machines rebooted successfully')
+                    status_table.add_row("All machines rebooted successfully")
                     exit()
 
 
-main()
+if __name__ == "__main__":
+    main()
